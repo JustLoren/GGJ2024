a28a155ad1d1e94931264b42fadae3271aabc78e
Loren <Loren.naegle@gmail.com>
2024-01-24 11:26:57 -0600

    Using the Father model for the main character. Have it properly blending between sad walk and happy walk based on current joy

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     private CharacterController controller;
-    public float speed = 5f;
+    public float depressedSpeed = 2f;
+    public float happySpeed = 5f;
     public float rotationSpeed = 10f; // Speed of rotation
     private Camera playerCamera; // Assign this in the Inspector
+    public Animator animator;
 
     void Start()
     {
@@ -27,8 +29,13 @@ public class Character : MonoBehaviour
         direction = playerCamera.transform.TransformDirection(direction);
         direction.y = 0; // Keep the movement horizontal
 
+        //Speed limit of 1 unit
+        if (direction.magnitude > 1f)
+            direction = direction.normalized;
+
         // Move the character
-        controller.Move(direction * speed * Time.deltaTime);
+        var realSpeed = Mathf.Lerp(depressedSpeed, happySpeed, JoyMeter.Instance.currentJoy);
+        controller.Move(direction * realSpeed * Time.deltaTime);
 
         // Rotate the character to face the moving direction
         if (direction != Vector3.zero)
@@ -36,5 +43,8 @@ public class Character : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+
+        animator.SetFloat("Speed", direction.magnitude);
+        animator.SetFloat("Joy", JoyMeter.Instance.currentJoy);
     }
 }

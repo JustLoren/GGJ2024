11211fe41b94f2a3024d2eb27cca186169dce4c8
Loren <Loren.naegle@gmail.com>
2024-01-28 00:41:13 -0600

    Added in remainder of soot spawn locations

--- Assets/Scripts/Soot Sprites/SootSpawner.cs
+++ Assets/Scripts/Soot Sprites/SootSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -18,36 +19,56 @@ public class SootSpawner : MonoBehaviour
         }
     }
 
+    private List<GameObject> spawnLocations = new();
+    private void OnEnable()
+    {
+        spawnLocations = GameObject.FindGameObjectsWithTag("Soot Spawn Location").ToList();
+    }
+
     private void Spawn()
     {
-        var spawnLocations = GameObject.FindGameObjectsWithTag("Soot Spawn Location");
+        var orderedLocations = spawnLocations.OrderBy(t => (Character.Instance.center - t.transform.position).magnitude).ToList();
 
-        var desiredLocation = spawnLocations
-                                .OrderBy(t => (Character.Instance.center - t.transform.position).magnitude)
+        var desiredLocation = orderedLocations
                                 .Skip(Random.Range(0,5))
-                                .FirstOrDefault(t => !IsVisibleToCamera(t.transform));
+                                .FirstOrDefault(t => IsObstructedFromCamera(t.transform));
 
         if (desiredLocation == null)
+        {
             desiredLocation = spawnLocations.OrderBy(t => Random.value).First();
+            Debug.Log("Couldn't find a suitable spawn location. Choosing randomly.");
+        } else
+        {
+            Debug.DrawLine(Camera.main.transform.position, desiredLocation.transform.position, Color.green, 1f);
+        }
 
         Instantiate(sootSpritePrefab, desiredLocation.transform.position, Quaternion.identity, this.transform);
         
         nextSpawnTime = Time.time + Random.Range(minSpawnInterval, maxSpawnInterval);
     }
 
-    bool IsVisibleToCamera(Transform target)
+    public LayerMask obstructionLayers;
+    bool IsObstructedFromCamera(Transform target)
     {
+        var pt = Camera.main.WorldToViewportPoint(target.position);
+        if (pt.x < 0 || pt.x > 1 || pt.y < 0 || pt.y > 1 || pt.z < 0)
+        {
+            return true;
+        }
+
         Vector3 direction = target.position - Camera.main.transform.position;
         RaycastHit hit;
-
-        if (Physics.Raycast(Camera.main.transform.position, direction, out hit, direction.magnitude))
+        Debug.DrawLine(Camera.main.transform.position, target.position, Color.yellow, 1f);
+        if (Physics.Raycast(Camera.main.transform.position, direction, out hit, direction.magnitude, obstructionLayers.value))
         {
             // Check if the hit object is not the target and is between the camera and the target
             if (hit.transform != target)
             {
+                Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red, 1f);
                 return true; // There is an obstruction
             }
         }
+
         return false; // No obstruction
     }
 }
